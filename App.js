996c32f303f89be16b77dbc1e25c797ab6cb372a
Lordphp1 <lordphp319@gmail.com>
2024-07-17 14:04:21 +0200

    first push

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <WebView
+        source={{ uri: 'https://nenkydata.com/auth/sign_in.php' }}
+        style={styles.webview}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  webview: {
+    flex: 1,
+  },
+});
